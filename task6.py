b384012c45c97df5ea533d1f4fcfa49d1e8c2861
Gopala-kannan <165189430+Gopala-kannan@users.noreply.github.com>
2025-04-29 05:54:25 +0530

    Update task6.py

--- task6.py
+++ task6.py
@@ -8,4 +8,9 @@ def remove_duplicate(list):
         if items not in unique_list:
             unique_list.append(items)
     return unique_list
-print(remove_duplicate([]))
+print(remove_duplicate([])) #function method
+
+# simple method
+list_input = input("Enter a Number: ")
+unique_list = list(set(list_input))
+print(unique_list)

b1b63c51583b0686af94a1f7a9e902e664eea818
Gopala-kannan <165189430+Gopala-kannan@users.noreply.github.com>
2025-04-29 06:00:35 +0530

    Update task8.py

--- task8.py
+++ task8.py
@@ -5,4 +5,11 @@ my_list = input("Enter a list of elements separated by spaces: ").split()
 element_name = input("Enter the element to count: ")
 count = element_count(my_list, element_name)
 print("The appears : ", element_name)
-print("times :", count)
+print("times :", count) #function method
+
+# simple method
+
+my_list = input("Enter a list of elements separated by spaces :")
+element = input("Enter a element to count :")
+count = my_list.count(element)
+print("count :", count)

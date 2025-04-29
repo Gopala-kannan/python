a5abad4288c557bb747e6a30aedc9812d1c4f2d8
Gopala-kannan <165189430+Gopala-kannan@users.noreply.github.com>
2025-04-29 06:19:37 +0530

    Update task17.py

--- task17.py
+++ task17.py
@@ -2,4 +2,15 @@ def digits(number):
     return sum(int(digit) for digit in str(number) if digit.isdigit())
 
 values = input("Enter a number : ")
-print("sum of digits : ", digits(values))
+print("sum of digits : ", digits(values)) # function method
+
+# simple method
+values = input("Enter a number : ")
+sum = 0
+for i in values:
+    if i.isdigit():
+        sum += int(i)
+print("sum of digits :", sum)
+
+# output = input_value = 526 = running 5+2+6 = 13 
+# output = so the digits value is 13

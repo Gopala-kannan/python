dbcb25e01ca014f4773a271239873898232ff2bb
Gopala-kannan <165189430+Gopala-kannan@users.noreply.github.com>
2025-04-29 05:57:08 +0530

    Update task7.py

--- task7.py
+++ task7.py
@@ -8,4 +8,10 @@ def common_elements(list1, list2):
     return common
 list1 = input("Enter the first list: ").split()
 list2 = input("Enter the second list: ").split()
-print("the common elements is : ", common_elements(list1, list2))
+print("the common elements is : ", common_elements(list1, list2)) #function method
+
+# simple method
+list_1 = input("Enter the first list : ")
+list_2 = input("Enter the second list : ")
+common = [i for i in list_1 if i in list_2]
+print(common)

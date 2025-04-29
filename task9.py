424339a9cc56547a507d425d722dbe7d66f7b898
Gopala-kannan <165189430+Gopala-kannan@users.noreply.github.com>
2025-04-29 06:05:39 +0530

    Update task9.py

--- task9.py
+++ task9.py
@@ -5,4 +5,10 @@ get_tuple =input("Enter a tuple of elements separated by spaces: ")
 element_name = input("Enter the element to count: ")
 count = count_tuples(get_tuple, element_name)
 print("The appears : ", element_name)
-print("times :", count)
+print("times :", count) #function method
+
+# simple method
+
+tuple_input =input("Enter a tuple of element : ")
+element = input("Enter the element to count : ")
+count = tuple_input.count(element)

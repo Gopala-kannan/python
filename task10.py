90ee8bfb2f4cc570abe50b18760a591e3fe51907
Gopala-kannan <165189430+Gopala-kannan@users.noreply.github.com>
2025-04-29 06:03:30 +0530

    Update task10.py

--- task10.py
+++ task10.py
@@ -5,6 +5,10 @@ def average_age(person_list):
     
 person_list = [("gopal", 21), ("kanna", 22), ("sajith", 20), ("mari", 17)]
 
-print(average_age(person_list))
+print(average_age(person_list)) #function method
 
+# simple method
 
+person_list = [("gopal", 21), ("kanna", 22), ("sajith", 20), ("mari", 17)]
+average_age = sum([age for name, age in person_list]) / len(person_list)
+print(average_age)

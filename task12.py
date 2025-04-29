7f208e3344f4b139989306a14a44b93f502d91c0
Gopala-kannan <165189430+Gopala-kannan@users.noreply.github.com>
2025-04-29 06:11:44 +0530

    Update task12.py

--- task12.py
+++ task12.py
@@ -6,4 +6,18 @@ def unique(str):
         character.add(char)
     return True
 str = input("Enter a value :")
-print(unique(str))
+print(unique(str)) # function method
+
+# simple method
+
+value = input("Enter a value :")
+for char in value:
+    if value.count(char) > 1:
+        print("False")
+        break
+    else:
+        print("True")
+
+# this code is find a unique elements 
+# output = gopal = one world not repeat so it is unique element
+# output = hello = one world repeact again and again "l l" so it is not unique element
